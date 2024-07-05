26fbb22c7f2e7372c5935ba8de3c1e08b2903a05
Yul Jeon <dalbitstream@gmail.com>
2024-07-05 14:34:40 +0900

    Initial commit

---
+++ src/main/java/practice/ezenstudy/EzenstudyApplication.java
@@ -0,0 +1,13 @@
+package practice.ezenstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzenstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzenstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/ezenstudy/EzenstudyApplicationTests.java
@@ -0,0 +1,14 @@
+package practice.ezenstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzenstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
