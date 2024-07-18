13adf3b49d8817d4f0a853e2b736f1549173318c
Yul Jeon <dalbitstream@gmail.com>
2024-07-18 09:49:05 +0900

    기능추가(강의): 페이징

--- src/main/java/practice/ezenstudy/lecture/LectureSearchParams.java
+++ src/main/java/practice/ezenstudy/lecture/LectureSearchParams.java
@@ -5,6 +5,28 @@ public record LectureSearchParams(
         String title,
         String teacherName,
         String category,
-        String studentId
+        String studentId,
+        Integer pageNumber,
+        Integer pageSize
 ) {
+    // 아래 코멘트 처리되어 있는 constructor와 동일한 기능의 compact constructor
+    public LectureSearchParams {
+        pageNumber = pageNumber == null ? 1 : pageNumber;
+        pageSize = pageSize == null ? 5 : pageSize;
+    }
+
+//    public LectureSearchParams(String sort, String title, String teacherName, String category, String studentId, Integer pageNumber, Integer pageSize) {
+//        this.sort = sort;
+//        this.title = title;
+//        this.teacherName = teacherName;
+//        this.category = category;
+//        this.studentId = studentId;
+//        this.pageNumber = pageNumber == null ? 1 : pageNumber;
+//        this.pageSize = pageSize == null ? 5 : pageSize;
+//    }
+
+    // Getter
+    public Integer offset() {
+        return (pageNumber - 1) * pageSize;
+    }
 }
