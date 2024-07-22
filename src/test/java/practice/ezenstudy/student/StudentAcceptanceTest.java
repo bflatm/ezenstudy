23308b4cb2eb89fa1e251a77eb87fbd844f2f8da
Yul Jeon <dalbitstream@gmail.com>
2024-07-22 19:10:50 +0900

    테스트(학생): RestAssured를 이용한 회원 가입 API 테스트 추가

---
+++ src/test/java/practice/ezenstudy/student/StudentAcceptanceTest.java
@@ -0,0 +1,32 @@
+package practice.ezenstudy.student;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import practice.ezenstudy.student.application.RegisterStudentRequest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StudentAcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    void 회원가입_성공() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new RegisterStudentRequest("doraemon@gmail.com", "도라에몽", "dora123"))
+                .when()
+                .post("/students")
+                .then().log().all()
+                .statusCode(200);
+    }
+}
