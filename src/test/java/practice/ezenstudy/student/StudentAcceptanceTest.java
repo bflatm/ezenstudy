2d116d8c1813e5f2397f3f2607d41bc235d4420c
Yul Jeon <dalbitstream@gmail.com>
2024-07-22 20:40:17 +0900

    fix(학생): 다른 테스트로 생긴 데이터의 영향을 받지 않도록, 데이터를 제거하는 sql이 매 테스트 전에 실행되도록 설정

--- src/test/java/practice/ezenstudy/student/StudentAcceptanceTest.java
+++ src/test/java/practice/ezenstudy/student/StudentAcceptanceTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import practice.ezenstudy.student.application.LoginRequest;
 import practice.ezenstudy.student.application.RegisterStudentRequest;
 
+@Sql("/truncate.sql")
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StudentAcceptanceTest {
